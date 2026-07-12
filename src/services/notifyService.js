279e83738913b5b227845a58900f177d6d2d135c
Landry Emil <emilngoyi89@gmail.com>
2026-07-12 11:03:37 +0100

    Ajout du bypass TLS pour nodemailer

--- src/services/notifyService.js
+++ src/services/notifyService.js
@@ -15,6 +15,10 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  // 👇 Voici le bloc ajouté pour contourner la sécurité de Render 👇
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 const COLORS = {
